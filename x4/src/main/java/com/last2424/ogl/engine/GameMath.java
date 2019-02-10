cb3d06dd485f75cc1868c546e860128aa1d32a81
last2424 <last24242@gmail.com>
2019-02-10 15:09:10 +0500

    Update Math

--- x4/src/main/java/com/last2424/ogl/engine/GameMath.java
+++ x4/src/main/java/com/last2424/ogl/engine/GameMath.java
@@ -5,20 +5,30 @@ import java.awt.Rectangle;
 import org.joml.Vector2f;
 
 import com.last2424.game.entities.Entity;
+import com.last2424.game.world.Tile;
+import com.last2424.game.world.Tile.TileConfig;
 
 public class GameMath {
 	
 	public static float GetDistance(Entity entityA, Entity entityB) {
-		return (float) Math.sqrt((entityA.GetPosition().y - entityB.GetPosition().y) * (entityA.GetPosition().y - entityB.GetPosition().y) + (entityA.GetPosition().x - entityB.GetPosition().x) * (entityA.GetPosition().x - entityB.GetPosition().x));
+		return (float)Math.sqrt((entityA.GetPosition().y - entityB.GetPosition().y) * (entityA.GetPosition().y - entityB.GetPosition().y) + (entityA.GetPosition().x - entityB.GetPosition().x) * (entityA.GetPosition().x - entityB.GetPosition().x));
 	}
 	
 	public static float GetDistance(Entity entityA, Vector2f point) {
-		return (float) Math.sqrt((entityA.GetPosition().y - point.y) * (entityA.GetPosition().y - point.y) + (entityA.GetPosition().x - point.x) * (entityA.GetPosition().x - point.x));
+		return (float)Math.sqrt((entityA.GetPosition().y - point.y) * (entityA.GetPosition().y - point.y) + (entityA.GetPosition().x - point.x) * (entityA.GetPosition().x - point.x));
 	}
 	
 	
 	
 	public static boolean Intersects(Rectangle rectA, Rectangle rectB) {
 		return rectA.intersects(rectB);
+	}
+	
+	public static boolean Intersects(Entity entity, Tile tile) {
+		if(tile.GetTileConfig() == TileConfig.SOLID) {
+			return new Rectangle((int)tile.GetPosition().x, (int)tile.GetPosition().y, (int)tile.GetSprite().size.x, (int)tile.GetSprite().size.y).intersects(new Rectangle((int)entity.GetPosition().x, (int)entity.GetPosition().y, (int)entity.GetSize().x, (int)entity.GetSize().y));
+		}
+		
+		return false;
 	}
 }
